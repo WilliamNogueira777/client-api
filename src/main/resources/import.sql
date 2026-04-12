ef8574e351076171d16e3270069111e6b67fb4bf
William Nogueira <wyllyam11@hotmail.com>
2026-04-12 16:59:53 -0300

    Clients implementation

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('João Silva', '12345678900', 3500.00, '1980-12-28', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('William Nogueira', '12345678911', 2600.00, '2000-12-28', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Sandra Regina', '12345678922', 1640.00, '1970-03-04', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Ronielton Nogueira', '12345678933', 1890.00, '2003-12-28', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Elen Almeida', '12345678944', 3800.00, '2000-06-15', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Milena Carvalho', '12345678955', 4800.00, '2000-05-06', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Jessica Santos', '12345678966', 3500.00, '2000-07-07', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Ester Alves', '12345678977', 4000.00, '2002-06-10', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Leticia Mascarenhas', '12345678988', 5000.00, '2000-08-09', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Caio Carvalho', '12345678999', 4200.00, '1997-08-08', 1);
